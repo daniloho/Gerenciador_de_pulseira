64c883bdaa1af9efb9e140abc0ea741e81881560
leollo98 <leo.sartorello98@gmail.com>
2019-11-03 15:42:41 -0300

    Initial commit

---
+++ app/src/main/java/nodomain/freeyourgadget/Pulseira/devices/huami/amazfitgtr/AmazfitGTRCoordinator.java
@@ -0,0 +1,67 @@
+/*  Copyright (C) 2017-2019 Andreas Shimokawa, Carsten Pfeiffer, Daniele
+    Gobbetti, João Paulo Barraca
+
+    This file is part of Gadgetbridge.
+
+    Gadgetbridge is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gadgetbridge is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+package nodomain.freeyourgadget.Pulseira.devices.huami.amazfitgtr;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nodomain.freeyourgadget.Pulseira.devices.InstallHandler;
+import nodomain.freeyourgadget.Pulseira.devices.huami.amazfitbip.AmazfitBipCoordinator;
+import nodomain.freeyourgadget.Pulseira.impl.GBDeviceCandidate;
+import nodomain.freeyourgadget.Pulseira.model.DeviceType;
+
+public class AmazfitGTRCoordinator extends AmazfitBipCoordinator {
+    private static final Logger LOG = LoggerFactory.getLogger(AmazfitGTRCoordinator.class);
+
+    @Override
+    public DeviceType getDeviceType() {
+        return DeviceType.AMAZFITGTR;
+    }
+
+    @NonNull
+    @Override
+    public DeviceType getSupportedType(GBDeviceCandidate candidate) {
+        try {
+            BluetoothDevice device = candidate.getDevice();
+            String name = device.getName();
+            if (name != null && name.equalsIgnoreCase("Amazfit GTR")) {
+                return DeviceType.AMAZFITGTR;
+            }
+        } catch (Exception ex) {
+            LOG.error("unable to check device support", ex);
+        }
+        return DeviceType.UNKNOWN;
+    }
+
+    @Override
+    public InstallHandler findInstallHandler(Uri uri, Context context) {
+        AmazfitGTRFWInstallHandler handler = new AmazfitGTRFWInstallHandler(uri, context);
+        return handler.isValid() ? handler : null;
+    }
+
+    @Override
+    public int getBondingStyle() {
+        return BONDING_STYLE_REQUIRE_KEY;
+    }
+}
